134f4cc21f8da079068adb77381a3cdaf3e7a72f
Sergio Cannobbio <sergio.cannobbio@gmail.com>
2025-08-29 16:11:06 -0400

    refactor: Force production database in settings

--- serca_project/settings.py
+++ serca_project/settings.py
@@ -113,21 +113,13 @@ WSGI_APPLICATION = 'serca_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    # Configuración para producción en Railway
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=os.getenv('DATABASE_URL'),
-            conn_max_age=600
-        )
-    }
+# Configuración para producción en Railway
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600
+    )
+}
 
 
 # Password validation
