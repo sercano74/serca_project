46876dc4d0aef91e135cb70ac1a3ff971490fe29
Sergio Cannobbio <sergio.cannobbio@gmail.com>
2025-08-29 23:51:44 -0400

    logo

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    email = forms.EmailField(label="Tu correo electrÃ³nico")
-    phone = forms.CharField(label="Tu telÃ©fono (opcional)", required=False, max_length=20)
+    email = forms.EmailField(label="Tu correo electrónico")
+    phone = forms.CharField(label="Tu teléfono (opcional)", required=False, max_length=20)
     message = forms.CharField(label="Tu mensaje", widget=forms.Textarea)
